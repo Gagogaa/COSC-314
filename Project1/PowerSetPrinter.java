e3fff30589d03246561e84d0fce8e9af2bdea6d2
Gregory Mann <gregory@laptob>
2019-09-05 17:30:48 -0400

    Done with project 1

--- Project1/PowerSetPrinter.java
+++ Project1/PowerSetPrinter.java
@@ -8,7 +8,9 @@
  * This program takes an Integer N and 'creates' a set A.
  * e.g. {1, 2, 3, ..., N}
  * This program then prints the powersets of A.
+ *
  * NOTE that the implementation does not actually create the set A. It just prints the powersets of A.
+ * It exploits the fact that the set is always sequential 1..N
  *
  */
 
@@ -53,13 +55,15 @@ class PowerSetPrinter
         // If the input is valid start the algorithm
         out.println('{');
 
+        // Bit shifting, in this case (1 << input), is a replacement for the power function when the base is two
+        // e.g. (2 ^ 3) == (1 << 3) == 8
+        int permutationCount = (1 << input);
+
         // Loop through all of the permutations of the set
-        // TODO this explanation still requires some work
-        // (1 << input) is a substitute for the power function when the base of the function is two
-        // (2 ^ 3) == (2 << 3)
-        for (int i = 0; i < (1 << input); i++)
+        // Just counting from 0 to the number of permutations will do, albeit in a weird order
+        for (int i = 0; i < permutationCount; i++)
         {
-            out.print(genSeq(i));
+            out.print(genSetString(i));
             // Check if we need to output the trailing comma
             if ((i + 1) < (1 << input))
                 out.println(',');
@@ -70,33 +74,32 @@ class PowerSetPrinter
     }
 
     /*
-     * Given a bit pattern produce the corresponding set
+     * Given a bit pattern produce the corresponding set as a String
      *
      * E.g. If seq is 0001 0101
-     * the bits are as follows
-     * 8 7 6 5  4 3 2 1
-     * ----------------
-     * 0 0 0 1  0 1 0 1
-     *
-     * {     5,   3,  1}
+     * Bit Number |  8 7 6 5  4 3 2 1
+     * -----------+-------------------
+     * Seq        |  0 0 0 1  0 1 0 1
+     * Output     | {      5,   3,  1}
      *
      */
     public static String
-    genSeq(int seq)
+    genSetString(int seq)
     {
         String res = "  {";
 
         // Checks each bit of seq and add the corresponding bit number to the result
-        // The stop condition is when the MSB of the test is larger than the number.
-        for (int shift = 0; (1 << shift) <= seq; shift++)
+        // The stop condition is when the most significant bit of the test is larger than seq.
+        for (int bit = 0; (1 << bit) <= seq; bit++)
         {
-            if ((seq & (1 << shift)) != 0)
+            // Use bit as a mask to check if the bit number should be printed out
+            if ((seq & (1 << bit)) != 0)
             {
                 // Append the BIT number to the sequence
-                res += Integer.toString(shift + 1);
+                res += Integer.toString(bit + 1);
 
                 // Check if we need to add in the trailing comma
-                if ((1 << (shift + 1)) <= seq)
+                if ((1 << (bit + 1)) <= seq)
                     res += ',';
             }
         }
